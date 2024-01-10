eb9fef019b116ce274390bdf9f4c817b0b734717
richardenrico <richardenrico02@gmail.com>
2024-01-10 19:11:48 +0800

    feat: config react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Router from 'Router'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+    const queryClient = new QueryClient()
+
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <Router />
-        </>
+        </QueryClientProvider>
     )
 }
 
